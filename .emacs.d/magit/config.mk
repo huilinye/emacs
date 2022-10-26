b06a9c38d17a56194469a2fa75e4b5d27f82fd1d
Huilin Ye <huilin.ye@uconn.edu>
2022-10-26 14:28:37 -0500

    upload corresponding libs

---
+++ .emacs.d/magit/config.mk
@@ -0,0 +1,7 @@
+
+
+
+LOAD_PATH  = -L /home/huilinye/.emacs.d/magit/lisp
+LOAD_PATH += -L /home/huilinye/.emacs.d/dash
+LOAD_PATH += -L /home/huilinye/.emacs.d/transient/lisp
+LOAD_PATH += -L /home/huilinye/.emacs.d/with-editor
---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,7 @@
+((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-log
+  (("--" "src/mem_pool/memory_pool.cc")))
+ (magit-tag nil))
