13a38baaf1ff3736a65bcee3ca57a5c507bb1772
root <root@PC-HUILINYE5820.global.cadence.com>
2024-05-23 21:46:44 -0500

    add package

---
+++ .emacs.d/elpa/helm-20240515.608/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20240515.608" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.8")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "44e9ce6857a1cc0159d1ee0b0fde53660fd0a6dc" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/helm-core-20240515.645/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20240515.645" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "08cfc12278991e5a62a3b7aeedb4adef420a3662" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
